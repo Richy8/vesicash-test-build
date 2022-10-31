4aade9b84ba51538d86dc1464d79c14c72b7b1ed
richy8 <menaelvisjones@gmail.com>
2022-10-31 08:29:50 +0100

    Updated env file

--- src/env.js
+++ src/env.js
@@ -13,4 +13,3 @@ export const VESICASH_PRIVATE_KEY =
   "v_live_n1W9ICPxHSzyvHYDP0izb8JDrC5vwlpG1X83LzmWLa1jmBgVGi";
 export const VESICASH_PUBLIC_KEY =
   "v_live_WFcxPqUBppDYn8hN78qqjvyX0crr5ki4Sr0C7DiSuyDfdLsYrL";
-modal;
