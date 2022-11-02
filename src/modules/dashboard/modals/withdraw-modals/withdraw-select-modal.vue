88a88e46c82c7ceb3c7f39768d42bb7d191f0255
Rilwan <akinyusuf5@gmail.com>
2022-11-02 16:27:45 +0100

    naira withdrawal tool tip updated

--- src/modules/dashboard/modals/withdraw-modals/withdraw-select-modal.vue
+++ src/modules/dashboard/modals/withdraw-modals/withdraw-select-modal.vue
@@ -9,9 +9,7 @@
       <div class="modal-cover-header">
         <div class="modal-cover-title">Select wallet to withdraw</div>
 
-        <div class="modal-cover-meta">
-          Please select the wallet you wish to withdraw
-        </div>
+        <div class="modal-cover-meta">Please select the wallet you wish to withdraw</div>
       </div>
     </template>
 
@@ -51,9 +49,7 @@
           class="btn btn-primary btn-md wt-100"
           @click="handleWalletSelection"
           :disabled="!wallet_type"
-        >
-          Continue
-        </button>
+        >Continue</button>
       </div>
     </template>
   </ModalCover>
@@ -104,8 +100,7 @@ export default {
     wallet_type: "naira",
     // tool_tip:
     //   "Wallet Withdrawal Fees <br> ₦500 for ₦0 - ₦500,000 <br> ₦1,000 for #500,001 - ₦1,000,000  <br> ₦2,000 (capped) for ₦1,000,001 and above",
-    tool_tip:
-      "<table class='table f-size-13 grey-700'><thead><tr><td>Withdrawal range</td><td>Charges</td></tr></thead><tbody><tr><td>₦0 - ₦500,000</td><td>₦500</td></tr><tr><td>₦500,001 - ₦1,000,000</td><td>₦1,000</td></tr><tr><td>₦1,000,001 and above</td><td>₦2,000 (capped)</td></tr></tbody></table>",
+    tool_tip: "<b>#50.00</b> will be charged for this withdrawal.",
   }),
 
   methods: {
