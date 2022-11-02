21ffc2f5467638e454ccf3c8bfb823e268760ebf
Rilwan <akinyusuf5@gmail.com>
2022-11-02 12:48:27 +0100

    withdrawal flow updates

--- src/modules/dashboard/modals/withdraw-modals/withdraw-confirm-modal.vue
+++ src/modules/dashboard/modals/withdraw-modals/withdraw-confirm-modal.vue
@@ -21,65 +21,38 @@
         <ModalListItem
           title="Amount to withdraw"
           :value="`${getWalletType == 'naira' ? 'N' : '$'}${
-            getWithdrawalMeta.amount
+            $money.addComma(getWithdrawalMeta.amount)
           }`"
         />
         <ModalListItem
           title="Withdrawal fee"
           :value="`${getWalletType == 'naira' ? 'N' : '$'}${
-            getWithdrawalMeta.fee
+            $money.addComma(getWithdrawalMeta.fee)
           }`"
         />
         <ModalListItem
           title="Total"
           :value="`${getWalletType == 'naira' ? 'N' : '$'}${
-            getWithdrawalMeta.total
+            $money.addComma(getWithdrawalMeta.total)
           }`"
         />
 
         <template v-if="getWalletType === 'naira'">
-          <ModalListItem
-            title="Bank name"
-            :value="getWithdrawalMeta.bank_name"
-          />
-          <ModalListItem
-            title="Account number"
-            :value="getWithdrawalMeta.account_no"
-          />
+          <ModalListItem title="Bank name" :value="getWithdrawalMeta.bank_name" />
+          <ModalListItem title="Account number" :value="getWithdrawalMeta.account_no" />
           <ModalListItem title="Account name" :value="getWithdrawalMeta.name" />
         </template>
 
         <template v-if="getWalletType === 'dollar'">
           <ModalListItem title="Country" :value="getWithdrawalMeta.country" />
           <!-- <ModalListItem title="Phone number" :value="getWithdrawalMeta.phone" /> -->
-          <ModalListItem
-            title="First name"
-            :value="getWithdrawalMeta.first_name"
-          />
-          <ModalListItem
-            title="Last name"
-            :value="getWithdrawalMeta.last_name"
-          />
-          <ModalListItem
-            title="Bank name"
-            :value="getWithdrawalMeta.bank_name"
-          />
-          <ModalListItem
-            title="Iban/Account no."
-            :value="getWithdrawalMeta.iban"
-          />
-          <ModalListItem
-            title="Swift code"
-            :value="getWithdrawalMeta.swift_code"
-          />
-          <ModalListItem
-            title="Sort code"
-            :value="getWithdrawalMeta.sort_code"
-          />
-          <ModalListItem
-            title="Bank Address"
-            :value="getWithdrawalMeta.bank_address"
-          />
+          <ModalListItem title="First name" :value="getWithdrawalMeta.first_name" />
+          <ModalListItem title="Last name" :value="getWithdrawalMeta.last_name" />
+          <ModalListItem title="Bank name" :value="getWithdrawalMeta.bank_name" />
+          <ModalListItem title="Iban/Account no." :value="getWithdrawalMeta.iban" />
+          <ModalListItem title="Swift code" :value="getWithdrawalMeta.swift_code" />
+          <ModalListItem title="Sort code" :value="getWithdrawalMeta.sort_code" />
+          <ModalListItem title="Bank Address" :value="getWithdrawalMeta.bank_address" />
         </template>
       </div>
     </template>
@@ -91,9 +64,7 @@
           class="btn btn-primary btn-md wt-100"
           ref="continue"
           @click="makeWithdrawal"
-        >
-          Continue
-        </button>
+        >Continue</button>
       </div>
     </template>
   </ModalCover>
@@ -130,7 +101,7 @@ export default {
         beneficiary_name: this.getWithdrawalMeta.name,
         bank_account_number: this.getWithdrawalMeta.account_no,
         bank_code: this.getWithdrawalMeta.bank_code,
-        amount: this.getWithdrawalMeta.total,
+        amount: this.getWithdrawalMeta.amount,
         currency: this.getWalletType === "naira" ? "NGN" : "USD",
         debit_currency: this.getWalletType === "naira" ? "NGN" : "USD",
       };
